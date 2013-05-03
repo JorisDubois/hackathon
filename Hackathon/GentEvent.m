7bf3f4ac6ab625083c5b68cf4ba510e373cab22f
JorisDubois <dubois.joris@gmail.com>
2013-05-03 15:50:34 +0200

    Models aangemaakt

---
+++ Hackathon/GentEvent.m
@@ -0,0 +1,14 @@
+//
+//  GentEvent.m
+//  Hackathon
+//
+//  Created by Hackathon on 03/05/13.
+//  Copyright (c) 2013 Team Bazinga. All rights reserved.
+//
+
+#import "GentEvent.h"
+
+@implementation GentEvent
+
+
+@end
